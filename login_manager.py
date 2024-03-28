c6ceb341374780430f3d9437ed7efe1adddf4c45
Cody Doucette <doucette@bu.edu>
2024-03-27 20:51:37 -0400

    Add solution

--- login_manager.py
+++ login_manager.py
@@ -26,7 +26,13 @@ class LoginManager:
     Resizes the table to be twice the size that it previously was.
     """
     def resize_table(self):
-        return
+        new_table = LoginManager(2 * len(self.table), self.max_load_factor)
+        for entry in self.table:
+            if entry == self.EMPTY or entry == self.REMOVED:
+                continue
+            new_table.register_user(entry[0], entry[1])
+        self.table = new_table.table
+        self.num_users = new_table.num_users
 
     """
     Register a new username and password combination by adding them to the table.
@@ -34,7 +40,50 @@ class LoginManager:
         Returns False if the table is full or if the user already exists.
     """
     def register_user(self, username, password):
-        return False
+        i = self.hash_func(username)
+
+        if self.num_users >= len(self.table):
+            # Table is full, and user might already exist.
+            return False
+
+        removed_spot = -1
+        num_checked = 0
+        while num_checked < len(self.table) and self.table[i] != self.EMPTY:
+            if self.table[i] == self.REMOVED:
+                if removed_spot == -1:
+                    removed_spot = i
+            elif self.table[i][0] == username:
+                return False
+
+            i = (i + 1) % len(self.table)
+            num_checked += 1
+
+        if self.table[i] == self.EMPTY:
+            # This user is not already in the table. Add the user
+            # in either the first empty or first removed spot.
+            if removed_spot != -1:
+                self.table[removed_spot] = (username, password)
+            else:
+                self.table[i] = (username, password)
+
+            self.num_users += 1
+
+            if self.num_users / len(self.table) > self.max_load_factor:
+                self.resize_table()
+
+            return True
+        elif num_checked >= len(self.table) and removed_spot != -1:
+            self.table[removed_spot] = (username, password)
+            self.num_users += 1
+            if self.num_users / len(self.table) > self.max_load_factor:
+                self.resize_table()
+            return True
+        else:
+            # This case shoulnd't be possible -- we already checked
+            # to make sure the table wasn't full, but if we've gotten
+            # here it means that we went through the entire table
+            # and didn't find any empty or removed spots.
+            return False
 
     """
     Deletes a user from the system by removing them from the table.
@@ -64,7 +113,26 @@ class LoginManager:
         Returns False if the given user cannot be found or if the password is incorrect.
     """
     def login(self, username, password):
-        return False
+        i = self.hash_func(username)
+        num_checked = 0
+        removed_spot = -1
+        while num_checked < len(self.table) and self.table[i] != self.EMPTY:
+            if self.table[i] == self.REMOVED or self.table[i][0] != username:
+                i = (i + 1) % len(self.table)
+                num_checked += 1
+            elif self.table[i][0] == username:
+                break
+
+        if num_checked >= len(self.table):
+            # Checked the whole table and user is not present.
+            return False
+
+        if self.table[i] == self.EMPTY:
+            # User is not present.
+            return False
+
+        # Only let user login if given password matches what's in table.
+        return self.table[i][1] == password
 
     """
     Merges two old accounts into a new account.
@@ -75,12 +143,61 @@ class LoginManager:
         new_account is a (username, password) tuple.
     """
     def merge_accounts(self, old_account1, old_account2, new_account):
-        return False
+        old_accounts_found = [False, False]
+
+        i = self.hash_func(old_account1[0])
+        num_checked = 0
+        while num_checked < len(self.table):
+            if self.table[i] == self.EMPTY:
+                return False
+            elif self.table[i] == self.REMOVED or self.table[i][0] != old_account1[0]:
+                i = (i + 1) % len(self.table)
+                num_checked += 1
+                continue
+            else:
+                break # found the account
+
+        i = self.hash_func(old_account2[0])
+        num_checked = 0
+        while num_checked < len(self.table):
+            if self.table[i] == self.EMPTY:
+                return False
+            elif self.table[i] == self.REMOVED or self.table[i][0] != old_account2[0]:
+                i = (i + 1) % len(self.table)
+                num_checked += 1
+                continue
+            else:
+                break # found the account
+
+        i = self.hash_func(new_account[0])
+        num_checked = 0
+        while num_checked < len(self.table):
+            if self.table[i] == self.EMPTY:
+                break
+            elif self.table[i] == self.REMOVED or self.table[i][0] != old_account2[0]:
+                i = (i + 1) % len(self.table)
+                num_checked += 1
+                continue
+            else:
+                return False # new account already exists
+
+        self.delete_user(old_account1[0])
+        self.delete_user(old_account2[0])
+        self.register_user(new_account[0], new_account[1])
+        return True
 
     def skip3_probe():
         return [
             # initial iteration:
             [ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
             # next iteration here:
-            [ ],
+            [ -1, -1, -1, 'dog', -1, -1, -1, -1, -1, -1, -1],
+            [ -1, -1, -1, 'dog', 'zebra', -1, -1, -1, -1, -1, -1],
+            [ -1, -1, 'cat', 'dog', 'zebra', -1, -1, -1, -1, -1, -1],
+            [ -1, -1, 'cat', 'dog', 'zebra', -1, 'giraffe', -1, -1, -1, -1],
+            [ -1, 'bird', 'cat', 'dog', 'zebra', -1, 'giraffe', -1, -1, -1, -1],
+            [ 'alpaca', 'bird', 'cat', 'dog', 'zebra', -1, 'giraffe', -1, -1, -1, -1],
+            [ 'alpaca', 'bird', 'cat', 'dog', 'zebra', -1, 'giraffe', -1, -1, 'alligator', -1],
+            [ 'alpaca', 'bird', 'cat', 'dog', 'zebra', -1, 'giraffe', 'elephant', -1, 'alligator', -1],
+            [ 'alpaca', 'bird', 'cat', 'dog', 'zebra', -1, 'giraffe', 'elephant', -1, 'alligator', 'baboon']
         ]
